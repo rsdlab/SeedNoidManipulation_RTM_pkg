0030c83ca5721c8f22eb07ae197f93149dd53e8d
soushi-masaki <130447084@ccmailg.meijo-u.ac.jp>
2019-03-18 17:32:40 +0900

    update

--- Component/SeedUpperBodyTestController/build/CMakeFiles/SeedUpperBodyTestController_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SeedUpperBodyTestController_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SeedUpperBodyTestController_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
